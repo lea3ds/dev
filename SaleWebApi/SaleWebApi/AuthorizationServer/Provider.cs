66ce6e9e572240e4c99b6eeccdb989a49aa2bc2d
Leandro Sarti <lsarti@elinpar.com>
2018-08-01 12:10:23 -0300

    x

---
+++ SaleWebApi/SaleWebApi/AuthorizationServer/Provider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SaleWebApi.AuthorizationServer
+{
+    public class Provider : OAuthAuthorizationServerProvider
+    {
+        public Provider()
+        {
+        }
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+            return base.ValidateClientAuthentication(context);
+        }
+
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var mail = context.UserName;
+            var password = context.Password;
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim(ClaimTypes.Name, mail));
+            context.Validated(identity);
+            return base.GrantResourceOwnerCredentials(context);
+        }
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
+            //return Task.FromResult<object>(null);
+
+            return base.TokenEndpoint(context);
+        }
+
+        public override Task GrantCustomExtension(OAuthGrantCustomExtensionContext context)
+        {
+            var code = context.Parameters.Get("code");
+            var uri = context.Parameters.Get("uri");
+            return base.GrantCustomExtension(context);
+        }
+
+        public override Task ValidateTokenRequest(OAuthValidateTokenRequestContext context)
+        {
+            return base.ValidateTokenRequest(context);
+        }
+    }
+}
