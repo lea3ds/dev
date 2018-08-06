e3450d53e25aae8f9bc86210a1906bb96973e7cf
Leandro Sarti <lsarti@elinpar.com>
2018-08-06 14:18:33 -0300

    ss

--- SaleApp/src/components/appWrapper/AuthChecker.js
+++ SaleApp/src/components/appWrapper/AuthChecker.js
@@ -1,6 +1,5 @@
 
 const Component = (props) => {
-    console.log("isAuthenticated",props.isAuthenticated)
 
     if (props.route.isPublic !== true && props.isAuthenticated !== true) {
         props.history.push(props.authPath.path);
