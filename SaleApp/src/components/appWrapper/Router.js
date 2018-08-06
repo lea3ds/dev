c19d64d24fcc17dc6738e28c19e96bd0e37f95a0
Leandro Sarti <lsarti@elinpar.com>
2018-08-06 14:58:10 -0300

    xxx

--- SaleApp/src/components/appWrapper/Router.js
+++ SaleApp/src/components/appWrapper/Router.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import AuthChecker from "./AuthChecker"
+import AuthenticationChecker from "./authenticationChecker"
 
 export const Component = (props) => {
     return <Switch>
         {props.routes.map((x, index) =>
             <Route
                 key={index}
-                exact={x.exact === false?false:true}
+                exact={x.exact === false ? false : true}
                 path={x.path}
                 //component = {e =>React.createElement(x.component, { ...e })}
-                render={e => <AuthChecker route={x} history = {props.history} authPath = {props.authPath}  isAuthenticated={props.isAuthenticated}  >{React.createElement(x.component, { ...e })}</AuthChecker>}
+                render={e =>
+                    <AuthenticationChecker
+                        route={x}
+                        history={props.history}
+                        authPath={props.authPath}
+                        isAuthenticated={props.isAuthenticated}>
+                        {React.createElement(x.component, {...e})}
+                    </AuthenticationChecker>
+                }
                 //render={e => React.createElement(x.component, { ...e, ...x.props })}
             />
         )}
