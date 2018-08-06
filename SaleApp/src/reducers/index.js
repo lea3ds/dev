66c6d9dec35beef37e0b4ce58cfd123299f3186f
Leandro Sarti <lsarti@elinpar.com>
2018-08-06 13:28:54 -0300

    sss

--- SaleApp/src/reducers/index.js
+++ SaleApp/src/reducers/index.js
@@ -2,18 +2,19 @@
 import {combineReducers} from 'redux';
 
 import appWraper from './appWraper';
-import data from './data';
-import dataArrays from './dataArrays';
-import pageIncome from './pageIncome';
+
+
+import {reducer as authenticationStore } from '../components/authentication';
+import {reducer as synchronizerStore } from '../components/synchronizer';
 
 import {reducer as paymentStore } from '../components/pages/Payment';
-import {reducer as authenticationStore } from '../components/authentication';
 
 export default combineReducers({
-    appWraper,
-    data,
-    dataArrays,
-    pageIncome,
-    paymentStore,
+
     authenticationStore,
+    appWraper,
+    dataArrays: synchronizerStore,
+
+    paymentStore,
+
 });
