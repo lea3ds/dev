bab3ce14db1438c583d9d72399301a5f2c9aefa1
Leandro Sarti <lsarti@elinpar.com>
2018-08-22 17:54:29 -0300

    remove SaleWebApi (rename)

--- SaleWebApi/SaleWebApi/Controllers/Resources/AccountChangePasswordRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SaleWebApi.Controllers.Resources
-{
-    public class AccountChangePasswordRequest
-    {
-        public string PasswordOld { get; set; }
-        public string PasswordNew { get; set; }
-    }
-
-}
